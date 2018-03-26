8a56af2d4d5e01b7a458877c0f5fdb6ef072d5cd
Antônio Augusto <antonioabritto@gmail.com>
2018-03-26 17:10:41 -0300

    instance methods example

--- lib/yaffle2/acts_as_yaffle.rb
+++ lib/yaffle2/acts_as_yaffle.rb
@@ -10,6 +10,14 @@ module Yaffle2
       def acts_as_yaffle(options = {})
         cattr_accessor :yaffle_text_field
         self.yaffle_text_field = (options[:yaffle_text_field] || :last_squawk).to_s
+
+        include Yaffle2::ActsAsYaffle::LocalInstanceMethods
+      end
+    end
+
+    module LocalInstanceMethods
+      def squawk(string)
+        write_attribute(self.class.yaffle_text_field, string.to_squawk)
       end
     end
   end
--- spec/lib/yaffle2/acts_as_yaffle_spec.rb
+++ spec/lib/yaffle2/acts_as_yaffle_spec.rb
@@ -3,4 +3,20 @@ require 'spec_helper'
 RSpec.describe 'ActsAsYaffle', type: :concern do
   it { expect(Hickwall.yaffle_text_field).to eq 'last_squawk' }
   it { expect(Wickwall.yaffle_text_field).to eq 'last_tweet' }
+
+  it 'Hickwalls squawk should populate last squawk' do
+    hickwall = Hickwall.new
+    expect { hickwall.squawk('Hello World') }
+      .to change { hickwall.last_squawk }
+      .from(nil)
+      .to('squawk! Hello World')
+  end
+
+  it 'Wickwalls squawk should populate last tweet' do
+    wickwall = Wickwall.new
+    expect { wickwall.squawk('Hello World') }
+      .to change { wickwall.last_tweet }
+      .from(nil)
+      .to('squawk! Hello World')
+  end
 end
