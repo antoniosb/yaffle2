bb7e5e072f4048e640dc6a8a10ecc3daeb49b1cf
Antônio Augusto <antonioabritto@gmail.com>
2018-03-26 17:18:27 -0300

    add database_cleaner

--- spec/dummy/spec/models/hickwall_spec.rb
+++ spec/dummy/spec/models/hickwall_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Hickwall, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'database cleaner works' do
+    expect { Hickwall.create! }
+      .to change { Hickwall.count }
+      .from(0)
+      .to(1)
+  end
+
+  it 'database cleaner works 2' do
+    expect { Hickwall.create!;Hickwall.create! }
+      .to change { Hickwall.count }
+      .from(0)
+      .to(2)
+  end
 end
--- spec/dummy/spec/models/wickwall_spec.rb
+++ spec/dummy/spec/models/wickwall_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Wickwall, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'database cleaner works' do
+    expect { Wickwall.create! }
+      .to change { Wickwall.count }
+      .from(0)
+      .to(1)
+  end
+
+  it 'database cleaner works 2' do
+    expect { Wickwall.create!;Wickwall.create! }
+      .to change { Wickwall.count }
+      .from(0)
+      .to(2)
+  end
 end
